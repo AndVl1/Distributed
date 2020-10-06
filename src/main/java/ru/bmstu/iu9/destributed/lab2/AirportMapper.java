f9d0841ddd6ce47919f4fad5a874ac2e950cfb46
Andrey <and.vladislavov@gmail.com>
2020-10-06 16:58:26 +0300

    Scripted auto-commit on change (2020-10-06 16:58:25) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/destributed/lab2/AirportMapper.java
+++ src/main/java/ru/bmstu/iu9/destributed/lab2/AirportMapper.java
@@ -1,4 +1,18 @@
 package ru.bmstu.iu9.destributed.lab2;
 
-public class SomeNameMapper {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.JobConf;
+
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class AirportMapper extends Mapper<LongWritable, Text, LongWritable, Text> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        super.map(key, value, context);
+    }
 }
