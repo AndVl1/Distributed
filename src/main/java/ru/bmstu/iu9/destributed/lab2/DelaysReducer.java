ac76d537e97d682599f3bdf5d88a6cead80a08e2
Andrey <and.vladislavov@gmail.com>
2020-10-07 13:12:04 +0300

    Scripted auto-commit on change (2020-10-07 13:12:04) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/destributed/lab2/DelaysReducer.java
+++ src/main/java/ru/bmstu/iu9/destributed/lab2/DelaysReducer.java
@@ -1,4 +1,9 @@
 package ru.bmstu.iu9.destributed.lab2;
 
-public class DelaysReduces {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class DelaysReducer extends Reducer<Text, IntWritable, Text, LongWritable> {
 }
