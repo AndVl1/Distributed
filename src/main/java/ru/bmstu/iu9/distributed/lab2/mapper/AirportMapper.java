f9ff6519403652dc11452b8114932ff4ed402b3c
Andrey <and.vladislavov@gmail.com>
2020-10-08 11:43:09 +0300

    Scripted auto-commit on change (2020-10-08 11:43:09) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/lab2/mapper/AirportMapper.java
+++ src/main/java/ru/bmstu/iu9/distributed/lab2/mapper/AirportMapper.java
@@ -3,10 +3,11 @@ package ru.bmstu.iu9.distributed.lab2.mapper;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import ru.bmstu.iu9.distributed.lab2.writable.Key;
 
 import java.io.IOException;
 
-public class AirportMapper extends Mapper<LongWritable, Text, LongWritable, Text> {
+public class AirportMapper extends Mapper<LongWritable, Text, Key, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException,
             InterruptedException {
--- src/main/java/ru/bmstu/iu9/distributed/lab2/mapper/ArrivalMapper.java
+++ src/main/java/ru/bmstu/iu9/distributed/lab2/mapper/ArrivalMapper.java
@@ -3,12 +3,14 @@ package ru.bmstu.iu9.distributed.lab2.mapper;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import ru.bmstu.iu9.distributed.lab2.writable.Key;
 
 import java.io.IOException;
 
-public class ArrivalMapper extends Mapper<LongWritable, Text, LongWritable, Text> {
+public class ArrivalMapper extends Mapper<LongWritable, Text, Key, Text> {
     @Override
-    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+    protected void map(LongWritable key, Text value, Context context) throws IOException,
+            InterruptedException {
 
     }
 
