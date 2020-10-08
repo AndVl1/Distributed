3d326b7f377bbd7a9ccfce5aaa8bb4d80e7f6508
Andrey <and.vladislavov@gmail.com>
2020-10-08 22:21:08 +0300

    Scripted auto-commit on change (2020-10-08 22:21:07) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/lab2/reducer/DelaysReducer.java
+++ src/main/java/ru/bmstu/iu9/distributed/lab2/reducer/DelaysReducer.java
@@ -1,13 +1,17 @@
 package ru.bmstu.iu9.distributed.lab2.reducer;
 
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 import ru.bmstu.iu9.distributed.lab2.writable.Key;
 
 import java.io.IOException;
+import java.util.Iterator;
 
-public class DelaysReducer extends Reducer<Key, Text, Text, LongWritable> {
-
+public class DelaysReducer extends Reducer<Key, Text, Text, Text> {
+    @Override
+    protected void reduce(Key key, Iterable<Text> values, Context context) throws IOException,
+            InterruptedException {
+        Iterator<Text> iter = values.iterator();
+        Text info = iter.next();
+    }
 }
