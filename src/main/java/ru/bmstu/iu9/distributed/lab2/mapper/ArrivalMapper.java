805c83f32336ad7de109807cc7a496fc8c1281c6
Andrey <and.vladislavov@gmail.com>
2020-10-08 19:45:39 +0300

    Scripted auto-commit on change (2020-10-08 19:45:39) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/lab2/mapper/ArrivalMapper.java
+++ src/main/java/ru/bmstu/iu9/distributed/lab2/mapper/ArrivalMapper.java
@@ -1,6 +1,7 @@
 package ru.bmstu.iu9.distributed.lab2.mapper;
 
 import au.com.bytecode.opencsv.CSVReader;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -9,14 +10,28 @@ import ru.bmstu.iu9.distributed.lab2.writable.Key;
 import java.io.IOException;
 import java.io.StringReader;
 
-public class ArrivalMapper extends Mapper<LongWritable, Text, Key, Text> {
+public class ArrivalMapper extends Mapper<LongWritable, Text, Key, DoubleWritable> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException,
             InterruptedException {
         CSVReader reader = new CSVReader(new StringReader(value.toString()), ',', '"', 1);
         String[] nextLine;
+        DoubleWritable flightDelay = new DoubleWritable();
         while ((nextLine = reader.readNext()) != null) {
-            int code;
+            int airportCode;
+            double delay;
+            try {
+                airportCode = Integer.getInteger(nextLine[14]);
+                delay = Double.parseDouble(nextLine[17]);
+                if (delay == 0.0) {
+                    continue;
+                }
+            } catch (Exception e) {
+                System.out.println(e.getLocalizedMessage());
+                continue;
+            }
+            flightDelay.set(delay);
+            context.write(new Key(airportCode, ARRIVAL_KEY), flightDelay);
         }
 
     }
