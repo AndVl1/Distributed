1533bf1618da8297f87f8500860882516e31204f
Andrey <and.vladislavov@gmail.com>
2020-10-09 10:18:13 +0300

    Scripted auto-commit on change (2020-10-09 10:18:13) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/lab2/partitioner/DataPartitioner.java
+++ src/main/java/ru/bmstu/iu9/distributed/lab2/partitioner/DataPartitioner.java
@@ -1,13 +1,19 @@
 package ru.bmstu.iu9.distributed.lab2.partitioner;
 
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Partitioner;
 import org.apache.hadoop.mapreduce.lib.partition.HashPartitioner;
+import ru.bmstu.iu9.distributed.lab2.writable.Key;
 
 public class DataPartitioner<K, V> extends Partitioner<K, V> {
 
     @Override
     public int getPartition(K k, V v, int numPartitions) {
-        if ()
+        if (!(k instanceof Key)) {
+
+        } else if (!(v instanceof Text)) {
+
+        }
         return 0;
     }
 }
