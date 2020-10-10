0fed5620312daaee257653e79a3194d3f29fe72d
Andrey <and.vladislavov@gmail.com>
2020-10-10 16:01:37 +0300

    Scripted auto-commit on change (2020-10-10 16:01:37) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/lab2/comparator/GroupingComparator.java
+++ src/main/java/ru/bmstu/iu9/distributed/lab2/comparator/GroupingComparator.java
@@ -13,4 +13,11 @@ public class GroupingComparator extends WritableComparator {
     public int compare(WritableComparable a, WritableComparable b) {
         return Integer.compare(((Key) a).getAirportId(), ((Key) b).getAirportId());
     }
+
+    @Override
+    public int compare(byte[] b1, int s1, int l1, byte[] b2, int s2, int l2) {
+        int id1 = readInt(b1, s1);
+        int id2 = readInt(b2, s2);
+        return Integer.compare(id1, id2);
+    }
 }
