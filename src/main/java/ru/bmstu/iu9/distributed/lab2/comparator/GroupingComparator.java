72661bce8b36264d93dfa39d615ccb59bf46eb37
Andrey <and.vladislavov@gmail.com>
2020-10-10 15:59:37 +0300

    Scripted auto-commit on change (2020-10-10 15:59:37) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/lab2/comparator/GroupingComparator.java
+++ src/main/java/ru/bmstu/iu9/distributed/lab2/comparator/GroupingComparator.java
@@ -1,9 +1,10 @@
 package ru.bmstu.iu9.distributed.lab2.comparator;
 
 import org.apache.hadoop.io.WritableComparator;
+import ru.bmstu.iu9.distributed.lab2.writable.Key;
 
 public class GroupingComparator extends WritableComparator {
     public GroupingComparator() {
-        super;
+        super(Key.class);
     }
 }
