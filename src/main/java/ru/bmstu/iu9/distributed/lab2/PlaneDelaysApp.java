bbf18817830a2acdfda1f12eecc07d78f5acf1ce
Andrey <and.vladislavov@gmail.com>
2020-10-10 12:14:12 +0300

    Scripted auto-commit on change (2020-10-10 12:14:12) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/lab2/PlaneDelaysApp.java
+++ src/main/java/ru/bmstu/iu9/distributed/lab2/PlaneDelaysApp.java
@@ -8,7 +8,6 @@ import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import ru.bmstu.iu9.distributed.lab2.mapper.AirportMapper;
 import ru.bmstu.iu9.distributed.lab2.mapper.ArrivalMapper;
-import ru.bmstu.iu9.distributed.lab2.partitioner.DataPartitioner;
 import ru.bmstu.iu9.distributed.lab2.reducer.DelaysReducer;
 import ru.bmstu.iu9.distributed.lab2.writable.Key;
 
@@ -26,7 +25,7 @@ public class PlaneDelaysApp {
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, ArrivalMapper.class);
 
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
-        job.setPartitionerClass(DataPartitioner.class);
+//        job.setPartitionerClass(DataPartitioner.class);
         job.setReducerClass(DelaysReducer.class);
         job.setMapOutputKeyClass(Key.class);
 
