9052b5829a0a555d1d2ffc529328d618b257fd02
Andrey <and.vladislavov@gmail.com>
2020-10-10 13:04:53 +0300

    Scripted auto-commit on change (2020-10-10 13:04:53) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/lab2/mapper/AirportMapper.java
+++ src/main/java/ru/bmstu/iu9/distributed/lab2/mapper/AirportMapper.java
@@ -1,29 +1,41 @@
 package ru.bmstu.iu9.distributed.lab2.mapper;
 
-import au.com.bytecode.opencsv.CSVReader;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import ru.bmstu.iu9.distributed.lab2.writable.Key;
 import java.io.IOException;
-import java.io.StringReader;
 
 public class AirportMapper extends Mapper<LongWritable, Text, Key, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException,
             InterruptedException {
         Text description = new Text();
-        CSVReader reader = new CSVReader(new StringReader(value.toString()), ',', '"', 1);
+
+        String[] lines = value.toString().split("\n");
+
         String[] nextLine;
-        while ((nextLine = reader.readNext()) != null) {
+        for(String line : lines) {
+            int firstComma = 0;
+            for (char symbol: line.toCharArray()){
+                if (symbol == ','){
+                    break;
+                }
+                firstComma++;
+            }
             int code;
             try {
-                code = Integer.getInteger(nextLine[0]);
+                code = Integer.getInteger(
+                        line.substring(0, firstComma)
+                                .replace('"', '\0')
+                );
             } catch (NumberFormatException e){
                 // This is header, so we must just continue to next line
                 continue;
             }
-            description.set(nextLine[1]);
+            description.set(
+                    line.substring(firstComma).replace('"', '\0')
+            );
             context.write(new Key(code, AIRPORT_KEY), description);
         }
     }
--- src/main/java/ru/bmstu/iu9/distributed/lab2/mapper/ArrivalMapper.java
+++ src/main/java/ru/bmstu/iu9/distributed/lab2/mapper/ArrivalMapper.java
@@ -1,39 +1,24 @@
 package ru.bmstu.iu9.distributed.lab2.mapper;
 
-import au.com.bytecode.opencsv.CSVReader;
-import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import ru.bmstu.iu9.distributed.lab2.writable.Key;
 import java.io.IOException;
-import java.io.StringReader;
 
 public class ArrivalMapper extends Mapper<LongWritable, Text, Key, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException,
             InterruptedException {
-        String data = value.toString();
-        CSVReader reader = new CSVReader(new StringReader(value.toString()), ',', '"', 1);
-
-        String[] lines = data.split("\n");
-        String nextLine;
+        String[] lines = value.toString().split("\n");
         Text flightDelay = new Text();
-        for(String line: lines) {
-            int firstComma = 0;
-            for (char symbol: line.toCharArray()){
-                if (symbol == ','){
-                    break;
-                }
-                firstComma++;
-            }
-            String info = line.substring(firstComma);
-            int code = Integer.getInteger(line.substring(0, firstComma).replace('"', ' '));
+        for (String line: lines) {
+            String[] fields = line.split(",");
             int airportCode;
             double delay;
             try {
-                airportCode = Integer.getInteger(nextLine[14]);
-                delay = Double.parseDouble(nextLine[17]);
+                airportCode = Integer.getInteger(fields[14]);
+                delay = Double.parseDouble(fields[17]);
                 if (delay == 0.0) {
                     continue;
                 }
