9b7bcfa466ad5decf5a90bb09fad3ca1542dcc00
Andrey <and.vladislavov@gmail.com>
2020-10-10 15:32:44 +0300

    Scripted auto-commit on change (2020-10-10 15:32:44) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/lab2/reducer/DelaysReducer.java
+++ src/main/java/ru/bmstu/iu9/distributed/lab2/reducer/DelaysReducer.java
@@ -4,14 +4,24 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 import ru.bmstu.iu9.distributed.lab2.writable.Key;
 import java.io.IOException;
+import java.util.Iterator;
 
 public class DelaysReducer extends Reducer<Key, Text, Text, Text> {
     @Override
     protected void reduce(Key key, Iterable<Text> values, Context context) throws IOException,
             InterruptedException {
-        
-        for (Text v: values) {
-            context.write(new Text(key.getDataIndicator() + ""), new Text(v.toString()));
+        Iterator<Text> vals = values.iterator();
+
+        String name = vals.next().toString();
+
+        if (!vals.hasNext()) {
+            // There are no delays, skip that
+            return;
         }
+
+        int maxDelay = 0;
+        int minDelay = 0;
+        int delaysCount = 0;
+        int sumDelay = 0;
     }
 }
