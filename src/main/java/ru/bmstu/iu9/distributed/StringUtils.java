0596ceaf858018d6b13d1968f4afd81362dc1291
Andrey <and.vladislavov@gmail.com>
2020-10-11 22:20:45 +0300

    Scripted auto-commit on change (2020-10-11 22:20:43) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/StringUtils.java
+++ src/main/java/ru/bmstu/iu9/distributed/StringUtils.java
@@ -13,16 +13,12 @@ public class StringUtils {
     public static AirportHelper trimCodeAndName(String line, char delimiter) {
         int firstComma = line.indexOf(delimiter);
         int code;
-        try {
-            String codeString = line
-                    .substring(0, firstComma)
-                    .replaceAll(SPEC_SYMBOLS, "");
-            code = Integer.parseInt(codeString);
-        } catch (Exception e){
-            // This is header, so we must just continue to next line
-            e.printStackTrace();
-            return null;
-        }
+
+        String codeString = line
+                .substring(0, firstComma)
+                .replaceAll(SPEC_SYMBOLS, "");
+        code = Integer.parseInt(codeString);
+
         String description = line.substring(firstComma + 1).replace('"', '\0');
         AirportHelper result = new AirportHelper();
         result.setCode(code);
