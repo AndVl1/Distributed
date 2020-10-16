0950ed1fafb386f8e8205efd9ea927e57dd46776
Andrey <and.vladislavov@gmail.com>
2020-10-17 02:49:09 +0300

    Scripted auto-commit on change (2020-10-17 02:49:09) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/lab2/mapper/ArrivalMapper.java
+++ src/main/java/ru/bmstu/iu9/distributed/lab2/mapper/ArrivalMapper.java
@@ -5,32 +5,32 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import ru.bmstu.iu9.distributed.StringUtils;
 import ru.bmstu.iu9.distributed.lab2.writable.Key;
+
 import java.io.IOException;
 
 public class ArrivalMapper extends Mapper<LongWritable, Text, Key, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException,
             InterruptedException {
-        String[] lines = value.toString().split("\n");
+        String line = value.toString();
         Text flightDelay = new Text();
-        for (String line: lines) {
 
-            String[] fields = StringUtils.trimSimpleCsv(line, CSV_DELIMITER);
-            int airportCode;
-            String delay;
-            try {
-                airportCode = Integer.parseInt(fields[CODE_INDEX]);
-                delay = fields[DELAY_INDEX];
-                if (delay.equals(ZERO_DELAY_STRING)) {
-                    continue;
-                }
-            } catch (Exception e) {
-//                System.out.println(TAG + " " + e.getMessage());
-                continue;
+        String[] fields = StringUtils.trimSimpleCsv(line, CSV_DELIMITER);
+        int airportCode;
+        String delay;
+        try {
+            airportCode = Integer.parseInt(fields[CODE_INDEX]);
+            delay = fields[DELAY_INDEX];
+            if (delay.equals(ZERO_DELAY_STRING)) {
+                return;
             }
-            flightDelay.set(delay);
-            context.write(new Key(airportCode, ARRIVAL_KEY), flightDelay);
+        } catch (Exception e) {
+//                System.out.println(TAG + " " + e.getMessage());
+            return;
         }
+        flightDelay.set(delay);
+        context.write(new Key(airportCode, ARRIVAL_KEY), flightDelay);
+
 
     }
 
