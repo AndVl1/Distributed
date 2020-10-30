7eaa62b1c10ce562c10d18fb75c952363216c4c9
Andrey <and.vladislavov@gmail.com>
2020-10-30 09:31:28 +0300

    Scripted auto-commit on change (2020-10-30 09:31:28) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/lab3/DelaysAppSpark.java
+++ src/main/java/ru/bmstu/iu9/distributed/lab3/DelaysAppSpark.java
@@ -6,4 +6,5 @@ import org.apache.spark.api.java.JavaSparkContext;
 public class DelaysAppSpark {
     SparkConf conf = new SparkConf().setAppName("Delays");
     JavaSparkContext sc = new JavaSparkContext(conf);
+    
 }
