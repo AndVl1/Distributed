a15f63a3502bc52af0e1534439636e66cc210df6
Andrey <and.vladislavov@gmail.com>
2020-10-31 02:12:54 +0300

    Scripted auto-commit on change (2020-10-31 02:12:54) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/lab3/DelaysAppSpark.java
+++ src/main/java/ru/bmstu/iu9/distributed/lab3/DelaysAppSpark.java
@@ -5,11 +5,15 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class DelaysAppSpark {
+
+    public static final String AIRPORTS_FILE_PATH = "airports.csv";
+    public static final String FLIGHTS_FILE_PATH = "flights.csv";
+
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("Delays");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaRDD<String> rawAirport = sc.textFile("airports.csv");
-        JavaRDD<String> rawFlights = sc.textFile("flights.csv");
+        JavaRDD<String> rawAirport = sc.textFile(AIRPORTS_FILE_PATH);
+        JavaRDD<String> rawFlights = sc.textFile(FLIGHTS_FILE_PATH);
 
         final String line = rawAirport.first();
         rawAirport = rawFlights.filter(s -> !s.equals(line));
