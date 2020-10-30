2e989260e62dfffec177925170f2207901927907
Andrey <and.vladislavov@gmail.com>
2020-10-31 02:58:51 +0300

    Scripted auto-commit on change (2020-10-31 02:58:51) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/CsvUtils.java
+++ src/main/java/ru/bmstu/iu9/distributed/CsvUtils.java
@@ -1,2 +1,14 @@
-package ru.bmstu.iu9.distributed;public class CsvUtils {
+package ru.bmstu.iu9.distributed;
+
+import org.apache.spark.api.java.JavaRDD;
+
+public class CsvUtils {
+    public static JavaRDD<String> getRddData(JavaRDD<String> data) {
+        return removeCsvTitleString(data);
+    }
+
+    private static JavaRDD<String> removeCsvTitleString(JavaRDD<String> data){
+        String firstCsvLine = data.first();
+        return data.filter(string -> !string.equals(firstCsvLine));
+    }
 }
