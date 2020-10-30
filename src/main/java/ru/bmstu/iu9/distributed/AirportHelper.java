45bd862e2df350f4e0f8f4f2fc54a7e691efd475
Andrey <and.vladislavov@gmail.com>
2020-10-30 10:25:05 +0300

    Scripted auto-commit on change (2020-10-30 10:25:05) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/AirportHelper.java
+++ src/main/java/ru/bmstu/iu9/distributed/AirportHelper.java
@@ -1,5 +1,7 @@
 package ru.bmstu.iu9.distributed;
 
+import static ru.bmstu.iu9.distributed.StringUtils.removeSpecSymbols;
+
 public class AirportHelper {
     private int code;
     private String description;
@@ -18,5 +20,21 @@ public class AirportHelper {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public static AirportHelper trimCodeAndName(String line, char delimiter) {
+        int firstComma = line.indexOf(delimiter);
+        int code;
+
+        String codeString = removeSpecSymbols(
+                line.substring(0, firstComma)
+        );
+        code = Integer.parseInt(codeString);
+
+        String description = line.substring(firstComma + 1).replaceAll(CSV_STRING_SYMBOL, "");
+        AirportHelper result = new AirportHelper();
+        result.setCode(code);
+        result.setDescription(description);
+        return result;
     }
 }
