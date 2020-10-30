a482b52de0c6694c4b1a1df96d3fec4d15715722
Andrey <and.vladislavov@gmail.com>
2020-10-30 10:01:46 +0300

    Scripted auto-commit on change (2020-10-30 10:01:46) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/lab3/DelaysAppSpark.java
+++ src/main/java/ru/bmstu/iu9/distributed/lab3/DelaysAppSpark.java
@@ -1,14 +1,19 @@
 package ru.bmstu.iu9.distributed.lab3;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class DelaysAppSpark {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("Delays");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        sc.textFile("airports.csv");
-        sc.textFile("flights.csv");
+        JavaRDD<String> rawAirport=sc.textFile("airports.csv");
+        JavaRDD<String> rawFlights=sc.textFile("flights.csv");
+
+        final String line=rawAirport.first();
+        rawAirport=rawFlights.filter(s->!s.equals(line));
+
         
     }
 }
