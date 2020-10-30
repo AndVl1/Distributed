cd62d401d6dd0d167979d290c766dbf9bf3036c4
Andrey <and.vladislavov@gmail.com>
2020-10-30 10:13:58 +0300

    Scripted auto-commit on change (2020-10-30 10:13:57) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/lab3/autorun.sh
+++ src/main/java/ru/bmstu/iu9/distributed/lab3/autorun.sh
@@ -1,1 +1,1 @@
-spark-submit --class ru.bmstu.ui9.distributed.lab3.DelaysAppSpark --master yarn-client --num-executors 3 <путь к jar файлу>
+spark-submit --class ru.bmstu.ui9.distributed.lab3.DelaysAppSpark --master yarn-client --num-executors 3 targer/hadoop-examples-1.0-SNAPSHOT.jar
