d28173c6c3ad6e67a6313850ec65d4366ba600c4
Andrey <and.vladislavov@gmail.com>
2020-10-31 10:51:37 +0300

    Scripted auto-commit on change (2020-10-31 10:51:37) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/lab3/DelaysAppSpark.java
+++ src/main/java/ru/bmstu/iu9/distributed/lab3/DelaysAppSpark.java
@@ -45,6 +45,7 @@ public class DelaysAppSpark {
                             delayedFlights++;
                         }
                     }
+                    
                 })
                 .map();
 
