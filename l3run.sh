39696251917fc9621a7ea1f62b61965e646ad40c
Andrey <and.vladislavov@gmail.com>
2020-10-31 11:08:24 +0300

    Scripted auto-commit on change (2020-10-31 11:08:24) by gitwatch.sh

--- l3run.sh
+++ l3run.sh
@@ -1,7 +1,4 @@
 hadoop fs -rm -f -r output
-
-spark-submit --class ru.bmstu.ui9.distributed.lab3.DelaysAppSpark --master yarn-client --num-executors 3 targer/hadoop-examples-1.0-SNAPSHOT.jar
-
+spark-submit --class ru.bmstu.iu9.distributed.lab3.DelaysAppSpark --master yarn-client --num-executors 3 targer/hadoop-examples-1.0-SNAPSHOT.jar
 rm -r output
-
 hadoop fs -copyToLocal output
