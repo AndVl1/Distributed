b245cc437beac49e37d541c1fdd36fb0e212adf9
Andrey <and.vladislavov@gmail.com>
2020-10-31 03:10:45 +0300

    Scripted auto-commit on change (2020-10-31 03:10:45) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/lab3/FlightData.java
+++ src/main/java/ru/bmstu/iu9/distributed/lab3/FlightData.java
@@ -3,4 +3,41 @@ package ru.bmstu.iu9.distributed.lab3;
 import scala.Serializable;
 
 public class FlightData implements Serializable {
+    private final String originId;
+    private final String destinationId;
+    private final int isCancelled;
+    private final double delay;
+
+    public FlightData(String originId, String destinationId, double delay) {
+        this.originId = originId;
+        this.destinationId = destinationId;
+        this.delay = delay;
+        isCancelled = NOT_CANCELLED;
+    }
+
+    public FlightData(String originId, String destinationId) {
+        this.originId = originId;
+        this.destinationId = destinationId;
+        this.delay = 0;
+        isCancelled = NOT_CANCELLED;
+    }
+
+    public String getOriginId() {
+        return originId;
+    }
+
+    public String getDestinationId() {
+        return destinationId;
+    }
+
+    public int getIsCancelled() {
+        return isCancelled;
+    }
+
+    public double getDelay() {
+        return delay;
+    }
+
+    private final static int CANCELLED = 1;
+    private final static int NOT_CANCELLED = 0;
 }
