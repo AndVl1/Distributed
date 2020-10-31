5f8fc41b196051f1230d455deb0d4f23f60fce3e
Andrey <and.vladislavov@gmail.com>
2020-10-31 11:05:50 +0300

    Scripted auto-commit on change (2020-10-31 11:05:50) by gitwatch.sh

--- l3run.sh
+++ l3run.sh
@@ -1,5 +1,3 @@
-mvn package
-
 hadoop fs -rm -f -r output
 
 spark-submit --class ru.bmstu.ui9.distributed.lab3.DelaysAppSpark --master yarn-client --num-executors 3 targer/hadoop-examples-1.0-SNAPSHOT.jar
@@ -7,5 +5,3 @@ spark-submit --class ru.bmstu.ui9.distributed.lab3.DelaysAppSpark --master yarn-
 rm -r output
 
 hadoop fs -copyToLocal output
-
-gitwatch .
