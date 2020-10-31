83c9133e862b2456a9235a93500dc59fb87dea55
Andrey <and.vladislavov@gmail.com>
2020-10-31 16:09:30 +0300

    Scripted auto-commit on change (2020-10-31 16:09:30) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/AirportHelper.java
+++ src/main/java/ru/bmstu/iu9/distributed/AirportHelper.java
@@ -1,6 +1,5 @@
 package ru.bmstu.iu9.distributed;
 
-import static ru.bmstu.iu9.distributed.StringUtils.CSV_STRING_SYMBOL;
 import static ru.bmstu.iu9.distributed.StringUtils.removeSpecSymbols;
 
 public class AirportHelper {
@@ -23,23 +22,23 @@ public class AirportHelper {
         this.description = description;
     }
 
-    public static AirportHelper trimCodeAndName(String line, char delimiter) {
-        return getAirportHelper(line, line.indexOf(delimiter));
+    public static AirportHelper trimCodeAndName(String csvLine, char delimiter) {
+        return getAirportHelper(csvLine, csvLine.indexOf(delimiter));
     }
 
-    public static AirportHelper trimCodeAndName(String line, String delimiter) {
-        return getAirportHelper(line, line.indexOf(delimiter));
+    public static AirportHelper trimCodeAndName(String csvLine, String delimiter) {
+        return getAirportHelper(csvLine, csvLine.indexOf(delimiter));
     }
 
-    private static AirportHelper getAirportHelper(String line, int firstIndexOfDelimiter) {
+    private static AirportHelper getAirportHelper(String csvLine, int firstIndexOfDelimiter) {
         int code;
 
         String codeString = removeSpecSymbols(
-                line.substring(0, firstIndexOfDelimiter)
+                csvLine.substring(0, firstIndexOfDelimiter)
         );
         code = Integer.parseInt(codeString);
 
-        String description = removeSpecSymbols(line.substring(firstIndexOfDelimiter + 1));
+        String description = removeSpecSymbols(csvLine.substring(firstIndexOfDelimiter + 1));
 
         AirportHelper result = new AirportHelper();
         result.setCode(code);
