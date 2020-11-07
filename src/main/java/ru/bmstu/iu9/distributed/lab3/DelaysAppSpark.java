d07dd66e104f0c86f073c19eeb67f5c7b588ba91
Andrey <and.vladislavov@gmail.com>
2020-11-07 17:22:06 +0300

    Scripted auto-commit on change (2020-11-07 17:22:06) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/lab3/DelaysAppSpark.java
+++ src/main/java/ru/bmstu/iu9/distributed/lab3/DelaysAppSpark.java
@@ -34,7 +34,7 @@ public class DelaysAppSpark {
 
         final Broadcast<Map<String, AirportData>> airportBroadcast = sparkContext.broadcast(airportsPairRdd.collectAsMap());
 
-        final LongAccumulator
+        final LongAccumulator total = 
 
         flightIdsToDataAccordance.reduceByKey((a, b) -> {
 
