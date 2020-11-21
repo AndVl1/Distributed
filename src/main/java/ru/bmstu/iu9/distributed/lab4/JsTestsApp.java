53b9d8082ceab5f5dac90bb088a207d0e79e0006
Andrey <and.vladislavov@gmail.com>
2020-11-21 18:28:59 +0300

    Scripted auto-commit on change (2020-11-21 18:28:59) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/lab4/JsTestsApp.java
+++ src/main/java/ru/bmstu/iu9/distributed/lab4/JsTestsApp.java
@@ -1,2 +1,8 @@
-package ru.bmstu.iu9.distributed.lab4;public class JsTestsApp {
+package ru.bmstu.iu9.distributed.lab4;
+
+public class JsTestsApp {
+
+    public static void main(String[] args) {
+
+    }
 }
