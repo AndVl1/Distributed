7b644bdb06d8792019570bce85d2548620a75b54
Andrey <and.vladislavov@gmail.com>
2020-11-21 18:54:00 +0300

    Scripted auto-commit on change (2020-11-21 18:54:00) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/lab4/MainHttp.java
+++ src/main/java/ru/bmstu/iu9/distributed/lab4/MainHttp.java
@@ -1,6 +1,14 @@
 package ru.bmstu.iu9.distributed.lab4;
 
+import akka.actor.ActorSystem;
+
 // TODO rename
-public class Server {
+public class MainHttp {
+
+    ActorSystem actorSystem;
     
+
+    public MainHttp(ActorSystem actorSystem) {
+        this.actorSystem = actorSystem;
+    }
 }
