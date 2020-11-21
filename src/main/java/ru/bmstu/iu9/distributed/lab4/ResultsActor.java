cc2439133c5a088b25c57892830df035d03c7ab3
Andrey <and.vladislavov@gmail.com>
2020-11-21 19:57:14 +0300

    Scripted auto-commit on change (2020-11-21 19:57:14) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/lab4/ResultsActor.java
+++ src/main/java/ru/bmstu/iu9/distributed/lab4/ResultsActor.java
@@ -2,7 +2,12 @@ package ru.bmstu.iu9.distributed.lab4;
 
 import akka.actor.AbstractActor;
 
+import java.util.HashMap;
+
 public class ResultsActor extends AbstractActor {
+    private final Map<String, /*testresult*/> testResults = new HashMap<>();
+
+
     @Override
     public Receive createReceive() {
         return null;
