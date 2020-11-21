4dff565ac9aec482f478710a01a2a294e6bdf880
Andrey <and.vladislavov@gmail.com>
2020-11-21 20:00:40 +0300

    Scripted auto-commit on change (2020-11-21 20:00:40) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/lab4/TestResult.java
+++ src/main/java/ru/bmstu/iu9/distributed/lab4/TestResult.java
@@ -1,4 +1,11 @@
 package ru.bmstu.iu9.distributed.lab4;
 
+import java.util.Map;
+
 public class TestResult {
+    private final Map<String, Boolean> results;
+
+    public TestResult(Map<String, Boolean> results) {
+        this.results = results;
+    }
 }
