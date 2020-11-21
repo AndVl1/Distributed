83d008914edefd8191a756c21b82d0625e25bce2
Andrey <and.vladislavov@gmail.com>
2020-11-21 18:35:21 +0300

    Scripted auto-commit on change (2020-11-21 18:35:21) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/lab4/RouterActor.java
+++ src/main/java/ru/bmstu/iu9/distributed/lab4/RouterActor.java
@@ -1,2 +1,10 @@
-package ru.bmstu.iu9.distributed.lab4;public class RouterActor {
+package ru.bmstu.iu9.distributed.lab4;
+
+import akka.actor.AbstractActor;
+
+public class RouterActor extends AbstractActor {
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
