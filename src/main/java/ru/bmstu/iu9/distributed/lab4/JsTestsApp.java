60f695995862022ea3653b2932ae3641346dcb46
Andrey <and.vladislavov@gmail.com>
2020-11-21 18:37:18 +0300

    Scripted auto-commit on change (2020-11-21 18:37:18) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/lab4/JsTestsApp.java
+++ src/main/java/ru/bmstu/iu9/distributed/lab4/JsTestsApp.java
@@ -1,8 +1,10 @@
 package ru.bmstu.iu9.distributed.lab4;
 
+import akka.actor.ActorSystem;
+
 public class JsTestsApp {
 
     public static void main(String[] args) {
-
+        ActorSystem 
     }
 }
