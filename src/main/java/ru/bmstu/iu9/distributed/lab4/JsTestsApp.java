e6ed1c285079394aa629457c47fdd1bb8f2f1589
Andrey <and.vladislavov@gmail.com>
2020-11-21 18:40:32 +0300

    Scripted auto-commit on change (2020-11-21 18:40:31) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/lab4/JsTestsApp.java
+++ src/main/java/ru/bmstu/iu9/distributed/lab4/JsTestsApp.java
@@ -2,12 +2,14 @@ package ru.bmstu.iu9.distributed.lab4;
 
 import akka.actor.ActorSystem;
 import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
 
 public class JsTestsApp {
 
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("JsTestsSystem");
         final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
         
     }
 }
