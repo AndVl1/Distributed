71831a4c926f655b45dfabf1de6dfbe77f6bffa0
Andrey <and.vladislavov@gmail.com>
2020-11-21 19:32:12 +0300

    Scripted auto-commit on change (2020-11-21 19:32:12) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/lab4/TestRequest.java
+++ src/main/java/ru/bmstu/iu9/distributed/lab4/TestRequest.java
@@ -1,5 +1,13 @@
 package ru.bmstu.iu9.distributed.lab4;
 
 public class TestRequest {
-    
+    private String packageId;
+    private String jsString;
+    private String functionName;
+
+    static class TestParams {
+        private String testName;
+        private String expectedResult;
+        private int[] params;
+    }
 }
