d72a2d69ae11944af573897b0bd653b8bd3644e2
Andrey <and.vladislavov@gmail.com>
2020-11-24 16:05:07 +0300

    Scripted auto-commit on change (2020-11-24 16:05:07) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/lab4/ResultMessage.java
+++ src/main/java/ru/bmstu/iu9/distributed/lab4/ResultMessage.java
@@ -1,5 +1,21 @@
 package ru.bmstu.iu9.distributed.lab4;
 
 public class ResultMessage {
-    public final String packageId
+    public final String packageId;
+    public final TestResult result;
+    public final boolean isRequest;
+
+    public ResultMessage(String packageId, TestResult result, boolean isRequest) {
+        this.packageId = packageId;
+        this.result = result;
+        this.isRequest = isRequest;
+    }
+
+    @Override
+    public String toString() {
+        return "ResultMessage{" +
+                "packageId='" + packageId + '\'' +
+                ", result=" + result +
+                '}';
+    }
 }
