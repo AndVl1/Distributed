d7130b334088f1f1b1933f48a0bc2643fb11c8d1
Andrey <and.vladislavov@gmail.com>
2020-11-24 16:51:50 +0300

    Scripted auto-commit on change (2020-11-24 16:51:49) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/lab4/RouterActor.java
+++ src/main/java/ru/bmstu/iu9/distributed/lab4/RouterActor.java
@@ -3,19 +3,38 @@ package ru.bmstu.iu9.distributed.lab4;
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.actor.Props;
+import akka.japi.pf.ReceiveBuilder;
+import akka.pattern.Patterns;
+import akka.routing.BalancingPool;
+import akka.util.Timeout;
+import scala.concurrent.Await;
+import scala.concurrent.Future;
+
+import java.time.Duration;
 
 public class RouterActor extends AbstractActor {
 
+    private static final int POOL_SIZE = 3;
+    public static final Timeout TIMEOUT = Timeout.create(Duration.ofSeconds(3));
     private ActorRef storageActor;
     private ActorRef executeActor;
 
     @Override
     public void preStart() {
-        storageActor = getContext().actorOf(Props.create())
+        storageActor = getContext().actorOf(Props.create(ResultsActor.class));
+        executeActor = getContext().actorOf(
+                new BalancingPool(POOL_SIZE).props(Props.create(TestExecutorActor.class))
+        );
     }
 
     @Override
     public Receive createReceive() {
-
+        return ReceiveBuilder.create()
+                .match(TestMessage.class, msg -> executeActor.tell(msg, self()))
+                .match(ResultsActor.class, msg -> storageActor.tell(msg, self()))
+                .match(RetrieveMessage.class, msg -> {
+                    Future<Object> future = Patterns.ask(storageActor, msg, TIMEOUT);
+                    sender().tell(Await.result(future, TIMEOUT.duration()), self());
+                }).build();
     }
 }
