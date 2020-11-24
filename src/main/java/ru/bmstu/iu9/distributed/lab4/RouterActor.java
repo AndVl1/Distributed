f1f25eb4021da53324ea171d547b71296247cceb
Andrey <and.vladislavov@gmail.com>
2020-11-24 16:40:49 +0300

    Scripted auto-commit on change (2020-11-24 16:40:49) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/lab4/RouterActor.java
+++ src/main/java/ru/bmstu/iu9/distributed/lab4/RouterActor.java
@@ -1,11 +1,17 @@
 package ru.bmstu.iu9.distributed.lab4;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Props;
 
 public class RouterActor extends AbstractActor {
+
+    private ActorRef storageActor;
+    private ActorRef executeActor;
+
     @Override
     public void preStart() {
-
+        storageActor = getContext().actorOf(Props.create())
     }
 
     @Override
