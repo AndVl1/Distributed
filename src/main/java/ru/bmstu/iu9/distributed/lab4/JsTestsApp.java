ebf65fa46a405509240a6c4e497c2a249f98bb09
Andrey <and.vladislavov@gmail.com>
2020-11-25 12:37:00 +0300

    Scripted auto-commit on change (2020-11-25 12:36:59) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/lab4/JsTestsApp.java
+++ src/main/java/ru/bmstu/iu9/distributed/lab4/JsTestsApp.java
@@ -2,11 +2,15 @@ package ru.bmstu.iu9.distributed.lab4;
 
 import akka.NotUsed;
 import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
+
+import java.util.concurrent.CompletionStage;
 
 public class JsTestsApp {
 
@@ -17,6 +21,8 @@ public class JsTestsApp {
         MainHttp instance = new MainHttp(system);
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
                 instance.getRoute().flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow, ConnectHttp.toHost("localhost", 8080), materializer);
         
     }
 }
