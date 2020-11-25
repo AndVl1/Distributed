dd2fecdb4efca5e27d875798822c068ac977154d
Andrey <and.vladislavov@gmail.com>
2020-11-25 12:42:45 +0300

    Scripted auto-commit on change (2020-11-25 12:42:45) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/lab4/actors/TestExecutorActor.java
+++ src/main/java/ru/bmstu/iu9/distributed/lab4/actors/TestExecutorActor.java
@@ -2,11 +2,20 @@ package ru.bmstu.iu9.distributed.lab4.actors;
 
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
+import ru.bmstu.iu9.distributed.lab4.TestMessage;
+import ru.bmstu.iu9.distributed.lab4.TestResult;
 
 public class TestExecutorActor extends AbstractActor {
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
+                .match(TestMessage.class, testMessage -> {
+                    TestResult result =
+                })
                 .build();
+    }
+
+    private TestResult runTests(TestMessage message) throws Exception{
+        
     }
 }
