e51c9d8c27b9b8bd406872b9fdeafc0d1cf44301
Andrey <and.vladislavov@gmail.com>
2020-11-25 12:37:31 +0300

    Scripted auto-commit on change (2020-11-25 12:37:31) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/distributed/lab4/JsTestsApp.java
+++ src/main/java/ru/bmstu/iu9/distributed/lab4/JsTestsApp.java
@@ -10,11 +10,12 @@ import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
+import java.io.IOException;
 import java.util.concurrent.CompletionStage;
 
 public class JsTestsApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ActorSystem system = ActorSystem.create("JsTestsSystem");
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
@@ -23,6 +24,10 @@ public class JsTestsApp {
                 instance.getRoute().flow(system, materializer);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow, ConnectHttp.toHost("localhost", 8080), materializer);
-        
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
     }
 }
